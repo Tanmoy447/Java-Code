a6d142283a4955d4b17822be58b199368661c991
Tanmoy447 <tanmoyadhikary612@gmail.com>
2023-08-04 00:45:02 +0530

    initial commit

---
+++ BasicJava/Methods_In_Java/factorial.java
@@ -0,0 +1,20 @@
+package Methods_In_Java;
+import java.util.*;
+
+class sum{
+    public int fact(int n){                     //Methods
+        if (n<=1)   return 1;
+        return n*fact(n-1);
+    }
+}
+public class factorial {
+    public static void main(String[] args) {
+        sum obj=new sum();                              //creating object of class
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the number :");
+        int n=sc.nextInt();
+        System.out.println("the factorial is : "+ obj.fact(n));     //creating obj of methods
+        sc.close();
+
+    }
+}
--- BasicJava/star.java
+++ BasicJava/star.java
@@ -1,10 +1,12 @@
-import java.util.*;
-public class star 
+import java.util.Scanner;
+//import java.utill.Methods_In_Java;
+public class star
 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the rows : ");
         int rows=sc.nextInt();
+        //factoial obj =new factorial();
         for(int i=1;i<=rows;i++)
         {
             for(int j=1;j<=rows-i;j++)
